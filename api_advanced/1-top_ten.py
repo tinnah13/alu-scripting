13dcdec756ede8943ebf1750fcf6567edfe45406
tinnah13 <a.umuhoza2@alustudent.com>
2026-06-29 00:25:09 -0700

    task done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,10 +6,11 @@ import requests
 def top_ten(subreddit):
     """Return number of subscribers if @subreddit is valid subreddit.
     if not return 0."""
-
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(
+        subreddit)
+    response = requests.get(subreddit_url, headers=headers,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         json_data = response.json()
